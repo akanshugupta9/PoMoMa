d767d423ee393210f86947a7ed6764bbd5d3122f
Akanshu Gupta <akanshugupta9@gmail.com>
2017-01-13 09:35:36 +0530

    Add page slider with sliding tabs

---
+++ app/src/main/java/com/example/akanshugupta9/pomoma/SampleFragmentPagerAdapter.java
@@ -0,0 +1,34 @@
+package com.example.akanshugupta9.pomoma;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by akanshugupta9 on 13/1/17.
+ */
+
+public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 3;
+    private String tabTitles[] = new String[] { "Tab1", "Tab2", "Tab3" };
+
+    public SampleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PageFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
